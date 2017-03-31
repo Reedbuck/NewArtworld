87dfc33d86285777a4f59591a46208cf50ce3640
Vpank <Garenemy@mail.ru>
2017-04-01 00:20:29 +0700

    Версия разработки 0.1
    Ромин лесс для навигации в шапке

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 <?php wp_footer(); ?>
 <?php get_footer( 'list' ); ?>
-        </div>
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,28 +1,63 @@
 <?php get_header(); ?>
 
-<section class="our_mission">
+<section class="found-mission">
     <div class="container">
+        <div class="col-md-12 title">
+            <h2 class="title__name">Миссия фонда</h2>
+        </div>
+        <div class="col-md-12 content">
+            <p>Фонд ArtWorld создан для работы в двух направлениях: искусство и социальные программы мы рассматриваем искусство, не как самоцель, а как средство стимулирования преобразовательных процессов в обществе и считаем, что с его помощью можно изменить не только социальную, а также экономическую и даже политическую среду, опираясь с одной стороны на творческих деятелей искусства и культуры, а с другой на поддержку и содействие политиков,бизнесменов и ученых. Основным проектом Фонда "Artworld" на сегодняшний день является 'Kids Republic', который направлен на создание единой молодежно-детской республики в странах Европейского союза, Восточной Европы, Азии и Америки. Цель проекта - объеденить детей и молодежь разных стран для сотрудничества, общения, обмена мнениями. Это поможет им совместно претворять в жизнь собственные планы.</p>
+            <button class="content__button">
+                <a href="http://#"></a>
+                Нас поддерживают
+            </button>
+        </div>
     </div>
 </section>
 
-<section class="funding_structure">
+<section class="funding-structure">
     <div class="container">
+        <div class="col-md-12 title"></div>
+            <h2 class="title__name">Структура фонда</h2>
+        <div class="col-md-12 content"></div>
+            <div class="col xs-12 md-6">
+                <p>Общее собрание членов отряда</p>
+            </div>
+            <div class="col xs-12 md-6">
+                <p>Совет по взаимодействию со СМИ</p>
+            </div>
+            <div class="col xs-6 md-2">
+                <p>Правление</p>
+            </div>
+            <div class="col xs-6 md-2">
+                <p>Студенческий совет</p>
+            </div>
+            <div class="col xs-6 md-2">
+                <p>Наблюдательный совет</p>
+            </div>
+            <div class="col xs-6 md-2">
+                <p>Паладины</p>
+            </div>
     </div>
 </section>
 
 <section class="projects">
     <div class="container">
-        
+        <div class="col-md-12 title"></div>
+        <div class="col-md-12 content"></div>
     </div>
 </section>
 
 <section class="partners">
     <div class="container">
+        <div class="col-md-12 title"></div>
+        <div class="col-md-12 content"></div>
     </div>
 </section>
-<section class="contcat">
+<section class="contact">
     <div class="container">
-        <div class="col-xs-12 col-sm-12"></div>
+        <div class="col-md-12 title"></div>
+        <div class="col-md-12 content"></div>
     </div>
 </section>
 
